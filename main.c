abb88f814ea0134b693375ad3f47e8591d0d1b26
zachthenerd169 <zachthenerd169@gmail.com>
2015-04-14 11:31:10 -0500

    More template code
    added ISR templates and used some functions from mpu6050 library

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#define YAW_THRESHOLD 0
+
 #include "matrix_display.h"
 
 struct led_matrix game;
@@ -5,5 +7,39 @@ game.leds = {{0}};
 
 int main(void)
 {
+	/* send init commands to accelerometer */
+	mpu6050_dmpInitalize();
+	mpu6050_dmpEnable();
 
+	sei();
+}
+
+/* ISR to refresh screen */
+ISR(/* timer */)
+{
+	show(game.leds);
+}
+
+/* ISR to update game */
+ISR(/* timer */)
+{
+	if(yaw > YAW_THRESHOLD)
+	{
+		/* move player right */
+	}
+	else if(yaw < -1*YAW_THRESHOLD)
+	{
+		/* move player left */
+	}
+	else
+	{
+		/* do not move player */
+	}
+}
+
+/* ISR to fetch sensor data */
+ISR(/* sensor data ready */)
+{
+	/* save values in global variables */
+	mpu6050_getRollPitchYaw(...);
 }
